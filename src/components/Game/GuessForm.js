220d8a5b73513f0d9d1b42213f1057fb1fd312ca
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2024-08-10 14:57:19 +0800

    limit the length of guess

--- src/components/Game/GuessForm.js
+++ src/components/Game/GuessForm.js
@@ -13,11 +13,15 @@ function GuessForm({ guess, setGuess, prevGuessList, setPrevGuessList }) {
         >
             <label for="guess-input">Enter guess:</label>
             <input
+                required
                 type="text"
                 id="guess-input"
                 value={guess}
                 minLength={5}
-                onChange={(e) => setGuess(e.target.value)}
+                maxLength={5}
+                pattern="[a-zA-Z]{5}"
+                title="5 letter word"
+                onChange={(e) => setGuess(e.target.value.toUpperCase())}
             />
         </form>
     );
